8440857af1c52cf8da8d0e4931d95f0574db5b9d
jhl1540 <144281579+jhl1540@users.noreply.github.com>
2024-06-04 16:50:25 +0900

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -186,31 +186,69 @@ def merge(all_peaks, connection_all, special_k):
     
     return candidate, subset
 
+# def draw_bodypose(canvas, candidate, subset, scale=1):
+#     stickwidth = 2
+#     dotwidth = 2
+#     limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
+#                [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
+#                [1, 16], [16, 18], [3, 17], [6, 18]]
+#
+#     colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+#               [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+#               [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+#     for i in range(18):
+#         for n in range(len(subset)):
+#             index = int(subset[n][i])
+#             if index == -1:
+#                 continue
+#             x, y = candidate[index][0:2]
+#             cv2.circle(canvas, (int(x/scale), int(y/scale)), int(dotwidth), colors[i], thickness=-1)
+#     for i in range(17):
+#         for n in range(len(subset)):
+#             index = subset[n][np.array(limbSeq[i]) - 1]
+#             if -1 in index:
+#                 continue
+#             cur_canvas = canvas.copy()
+#             Y = candidate[index.astype(int), 0]/scale
+#             X = candidate[index.astype(int), 1]/scale
+#             mX = np.mean(X)
+#             mY = np.mean(Y)
+#             length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
+#             angle = math.degrees(math.atan2(X[0] - X[1], Y[0] - Y[1]))
+#             polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), int(stickwidth)), int(angle), 0, 360, 1)
+#             cv2.fillConvexPoly(cur_canvas, polygon, colors[i])
+#             canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
+#     return canvas
+
+
 def draw_bodypose(canvas, candidate, subset, scale=1):
     stickwidth = 2
     dotwidth = 2
-    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
-               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
+    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
                [1, 16], [16, 18], [3, 17], [6, 18]]
 
-    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
-              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0],
+              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255],
               [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+
     for i in range(18):
         for n in range(len(subset)):
             index = int(subset[n][i])
             if index == -1:
                 continue
             x, y = candidate[index][0:2]
-            cv2.circle(canvas, (int(x/scale), int(y/scale)), int(dotwidth), colors[i], thickness=-1)
+            cv2.circle(canvas, (int(x / scale), int(y / scale)), int(dotwidth), colors[i], thickness=-1)
+            cv2.putText(canvas, str(i), (int(x / scale), int(y / scale)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[i], 1, cv2.LINE_AA)
+
     for i in range(17):
         for n in range(len(subset)):
             index = subset[n][np.array(limbSeq[i]) - 1]
             if -1 in index:
                 continue
             cur_canvas = canvas.copy()
-            Y = candidate[index.astype(int), 0]/scale
-            X = candidate[index.astype(int), 1]/scale
+            Y = candidate[index.astype(int), 0] / scale
+            X = candidate[index.astype(int), 1] / scale
             mX = np.mean(X)
             mY = np.mean(Y)
             length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
@@ -218,7 +256,9 @@ def draw_bodypose(canvas, candidate, subset, scale=1):
             polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), int(stickwidth)), int(angle), 0, 360, 1)
             cv2.fillConvexPoly(cur_canvas, polygon, colors[i])
             canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
+
     return canvas
+
 
 def draw_part(canvas, all_peaks, ID, scale=1):
     dotwidth = 8
