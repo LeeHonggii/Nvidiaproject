527c82347123b9aa2123f308f68aa33d7ff27f42
phantasy77 <phantasy@paran.com>
2024-06-13 14:27:09 +0900

    interpolation

--- video_editor/adjust.py
+++ video_editor/adjust.py
@@ -15,7 +15,7 @@ def calculate_signed_angle(v1, v2):
     length_v2 = length(v2)
 
     cos_theta = dot_prod / (length_v1 * length_v2)
-    print(cos_theta, dot_prod, length_v1, length_v2)
+    # print(cos_theta, dot_prod, length_v1, length_v2)
     if cos_theta > 1:
         cos_theta = 1
     angle_radians = acos(cos_theta)
@@ -38,14 +38,24 @@ def find_intersection(x1, y1, a, x2, y2, b):
     return solutions
 
 
+def vector_interpolation(v1, v2):
+    # diff_x, diff_y, ratio, angle, px, py, pw, ph, _, _, _, _ = vector_difference(prev_vector, curr_vector)
+    # print("vector_difference called", prev_vector, curr_vector)
+    # px, py, pw, ph = prev_vector
+    # cx, cy, cw, ch = clurr_vector
+    # px2, py2 = px + pw, py + ph
+    # cx2, cy2 = cx + cw, cy + ch
+    # adjust_vector = int(px + (diff_x / 2)), int(py + (diff_y / 2)), int(pw * ((1/ratio) ** 0.5)), int(ph * ((1/ratio) ** 0.5))
+    x1, y1, w1, h1 = v1
+    x2, y2, w2, h2 = v2
+    x12, y12 = x1 + w1, y1 + h1
+    x22, y22 = x2 + w2, y2 + h2
+    ix1, iy1, ix2, iy2 = (x1 + x2) / 2, (y1 + y2) / 2, (x12 + x22) / 2, (y12 + y22) / 2
+
+    return [ix1, iy1, ix2 - ix1, iy2 - iy1]
+
+
 def get_adjusted_clip(clip, v1, v2):
-    if v1 == v2:
-        return clip
-
-    width, height = clip.size
-    center_x = int(width / 2)
-    center_y = int(height / 2)
-
     x1, y1, w1, h1 = v1
     x2, y2, w2, h2 = v2
     x12, y12 = x1 + w1, y1 + h1
@@ -57,19 +67,26 @@ def get_adjusted_clip(clip, v1, v2):
 
     diff_x = x2 - x1
     diff_y = y2 - y1
-    distance = dist(diff_x, diff_y)
+    # distance = dist(diff_x, diff_y)
     length_1 = dist(w1, h1)
     length_2 = dist(w2, h2)
     ratio = length_1 / length_2
-    print("(", x1, y1, w1, h1, ") to (", x2, y2, w2, h2, ")")
-    print(f"From [p1({x1}, {y1}), v1({w1}, {h1})] To [p2({x2}, {y2}), v2({w2}, {h2})]")
-    print(f"distance: {distance:.2f}, length_1: {length_1:.2f}, length_2: {length_2:.2f}, ratio: {ratio:.2f}")
+    rotate_angle = calculate_signed_angle((w1, h1), (w2, h2))
+    # return diff_x, diff_y, ratio, rotate_angle, x1, y1, w1, h1, x2, y2, w2, h2
+    # diff_x, diff_y, ratio, rotate_angle, x1, y1, w1, h1, x2, y2, w2, h2 = vector_difference(v1, v2)
+
+    # print(f"From [p1({x1}, {y1}), v1({w1}, {h1})] To [p2({x2}, {y2}), v2({w2}, {h2})]")
+    # print(f"diff_x: {diff_x:.2f}, diff_y: {diff_y:.2f}, ratio: {ratio:.2f}, angle: {rotate_angle:.2f}")
+    # # print(f"distance: {distance:.2f}, diff_x: {diff_x:.2f}, diff_y: {diff_y:.2f}, ratio: {ratio:.2f}, angle: {rotate_angle:.2f}")
+
+    width, height = clip.size
+    center_x = int(width / 2)
+    center_y = int(height / 2)
 
     # 1) moving
     clip = clip.on_color(size=(width, height), color=(0, 0, 0), pos=(-diff_x, -diff_y))
 
     # 2) rotation
-    rotate_angle = calculate_signed_angle((w1, h1), (w2, h2))
     distance_from_center = dist(x1 - center_x, y1 - center_y)
     if distance_from_center == 0:
         rotated_move_x = 0
@@ -78,8 +95,8 @@ def get_adjusted_clip(clip, v1, v2):
         opposite_side = distance_from_center * sin(radians((abs(rotate_angle) / 2)))
         near_side = distance_from_center * cos(radians((abs(rotate_angle) / 2)))
         solution = find_intersection(x1, y1, opposite_side, center_x, center_y, near_side)
-        print(f"Signed angle between vectors: {rotate_angle:.2f} degrees")
-        print(solution)
+        # print(f"Signed angle between vectors: {rotate_angle:.2f} degrees")
+        # print(solution)
         # print(f"distance_from_center: {distance_from_center:.2f} opposite_side: {opposite_side:.2f}, near_side: {near_side:.2f}")
         # print(solution)
         # print(f"rotated_move_x: {rotated_move_x:.2f}, rotated_move_x: {rotated_move_y:.2f}")
@@ -93,7 +110,7 @@ def get_adjusted_clip(clip, v1, v2):
     clip = clip.rotate(rotate_angle)
 
     rotated_window_width, rotated_window_height = clip.size
-    print(f"rotated window size: {rotated_window_width} x {rotated_window_height}")
+    # print(f"rotated window size: {rotated_window_width} x {rotated_window_height}")
     window_move_x = abs(rotated_window_width - width) / 2
     window_move_y = abs(rotated_window_height - height) / 2
 
@@ -106,8 +123,8 @@ def get_adjusted_clip(clip, v1, v2):
     # 4) total correction
     total_move_x = (rotated_move_x + window_move_x) * ratio + scaled_move_x
     total_move_y = (rotated_move_y + window_move_y) * ratio + scaled_move_y
-    print(f"rotated_move_x: {rotated_move_x:.1f} + window_move_x: {window_move_x:.1f} + scaled_move_x: {scaled_move_x:.1f} = total_move_x: {total_move_x:.1f}")
-    print(f"rotated_move_y: {rotated_move_y:.1f} + window_move_y: {window_move_y:.1f} + scaled_move_y: {scaled_move_y:.1f} = total_move_y: {total_move_y:.1f}")
+    # print(f"rotated_move_x: {rotated_move_x:.1f} + window_move_x: {window_move_x:.1f} + scaled_move_x: {scaled_move_x:.1f} = total_move_x: {total_move_x:.1f}")
+    # print(f"rotated_move_y: {rotated_move_y:.1f} + window_move_y: {window_move_y:.1f} + scaled_move_y: {scaled_move_y:.1f} = total_move_y: {total_move_y:.1f}")
 
     clip = clip.on_color(size=(width, height), color=(0, 0, 0), pos=(-total_move_x, -total_move_y))
     clip = clip.resize(newsize=(width, height))
@@ -160,8 +177,8 @@ if __name__ == "__main__":
     ratio = 2.0
     v3 = x1, y1, w1*ratio, h1*ratio
     temp_clip = get_adjusted_clip(clip2, v1, v2)
-    adjust_clip2 = get_adjusted_clip(temp_clip, v3, v1)
-    # adjust_clip2 = get_adjusted_clip(clip2, v3, v2)
+    adjust_clip1 = get_adjusted_clip(temp_clip, v3, v1)
+    adjust_clip2 = get_adjusted_clip(clip2, v3, v2)
 
     final_clip = concatenate_videoclips([clip1, adjust_clip1, adjust_clip2])
     final_clip.write_videofile("data/adjust_video.mp4", codec='libx264', audio_codec='aac')
--- video_editor/video_editor.py
+++ video_editor/video_editor.py
@@ -3,11 +3,9 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideo
 import json
 import random
 import time
-from adjust import get_adjusted_clip
+from adjust import get_adjusted_clip, vector_interpolation
 
 begin_time = time.time()
-
-dissolve = 0.0
 
 with open("data/output.json", "r", encoding="utf-8") as file:
     json_string_from_file = file.read()
@@ -22,8 +20,9 @@ current_stream = parameter["meta_info"]["first_stream"]
 num_cross = parameter["meta_info"]["num_cross"]
 folder_path = parameter["meta_info"]["folder_path"]
 cross_list = parameter["cross_points"]
-sclip_list = []
-tclip_list = []
+scene_list = parameter["scene_list"]
+stream_list = []
+target_list = []
 
 streams = parameter["streams"]
 if num_stream != len(streams):
@@ -46,36 +45,48 @@ for stream in streams:
     filename = folder_path + stream["file"]
     clip = load_video_clip(filename)
     if clip:
-        sclip_list.append(clip)
+        stream_list.append(clip)
         # if round(clip.fps, 2) != frame_rate:
         #     print(f"ALERT!!! invalide frame rate in file \"{filename}\" {clip.fps} vs parameter[\"frame_rate\"]: {frame_rate}")
         # else:
         #     print(filename)
 
         if stream["start"] != 0 or stream["end"] != 0:
-            sclip_list[-1].subclip(stream["start"], stream["end"])
+            stream_list[-1].subclip(stream["start"], stream["end"])
             print("ALERT!!! we do not support subclip so far")
 
 
-if num_stream != len(sclip_list):
-    print(f"ALERT!!! invalid stream number. num_stream: {num_stream} vs len(sclip_list): {len(sclip_list)}")
-    num_stream = len(sclip_list)    # can we continue?
+if num_stream != len(stream_list):
+    print(f"ALERT!!! invalid stream number. num_stream: {num_stream} vs len(stream_list): {len(stream_list)}")
+    num_stream = len(stream_list)    # can we continue?
 
-frame_rate = sclip_list[0].fps
+# for j in range(num_stream):
+#     begin = 0
+#     for i in range(len(scene_list[j])):
+#         end = scene_list[j][i]
+#         print(end)
+#         clip = stream_list[j].subclip(begin, end)
+#         output_path = f'data/scene_test_{j}_{i}_{end:.4f}.mp4'
+#         clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
+#         begin = end
+# quit()
+
+frame_rate = stream_list[0].fps
 for i in range(num_stream):
     if i == 0:
-        frame_rate = sclip_list[i].fps
-    elif frame_rate != sclip_list[i].fps:
-        print(f"ALERT!!! different frame stream 0: {frame_rate} vs stream {i}: sclip_list[i].fps")
+        frame_rate = stream_list[i].fps
+    elif frame_rate != stream_list[i].fps and parameter["meta_info"]["metric"] == "frame":
+        print(f"ALERT!!! different frame stream 0: {frame_rate} vs stream {i}: stream_list[i].fps")
         quit()
 
 if "metric" not in parameter["meta_info"] or parameter["meta_info"]["metric"] == "frame":
     frame_gap = 1 / frame_rate
+    print("==================")
     for cross in cross_list:
-        cross["time"] = cross["frame_id"] * frame_gap
+        cross["time_stamp"] = cross["frame_id"] * frame_gap
 
 last_cross = {
-    "time": init_time + total_duration,
+    "time_stamp": init_time + total_duration,
     "next_stream": cross_list[-1]["next_stream"],
     "vector_pairs": [
         {
@@ -86,7 +97,7 @@ last_cross = {
 }
 cross_list.append(last_cross)
 if num_cross + 1 != len(cross_list):
-    print("!!!INVLIDS cross number:", num_cross)
+    print("ALERT!!! invalid cross number:", num_cross)
 else:
     num_cross += 1
 print(cross_list)
@@ -94,33 +105,48 @@ print(cross_list)
 start_time = init_time
 # move_x = 0
 # move_y = 0
-current_vector = [0, 0, 0, 0]
-next_vector = [0, 0, 0, 0]
+prev_vector = [0, 0, 0, 0]
+curr_vector = [0, 0, 0, 0]
 
 for i in range(num_cross):
     current_time = cross_list[i]["time_stamp"]
-    clip = sclip_list[current_stream].subclip(start_time, current_time)
-    clip = get_adjusted_clip(clip, current_vector, next_vector)
-    tclip_list.append(clip)
-    print(f"stream: {current_stream}, start: {start_time:.1f}, end: {current_time}, cv: {current_vector}, nv: {next_vector}")
+
+    if prev_vector == curr_vector:
+        clip = stream_list[current_stream].subclip(start_time, current_time)
+        print(f"{len(target_list)}th clip added. stream: {current_stream}, start: {start_time:.1f}, end: {current_time}, cv: {prev_vector}, nv: {curr_vector}")
+        target_list.append(clip)
+    else:
+        if len(target_list) == 0:
+            print("ALERT!!! no more prev clip")
+        prev_clip = target_list.pop()
+        curr_clip = stream_list[current_stream].subclip(start_time, current_time)
+
+        interpolated_vector = vector_interpolation(prev_vector, curr_vector)
+
+        prev_clip = get_adjusted_clip(prev_clip, interpolated_vector, prev_vector)
+        curr_clip = get_adjusted_clip(curr_clip, interpolated_vector, curr_vector)
+        print(f"{len(target_list)}th clip adjusted. stream: {current_stream}, start: {start_time:.1f}, end: {current_time}, pv: {prev_vector}, iv: {interpolated_vector}")
+        target_list.append(prev_clip)
+        print(f"{len(target_list)}th clip added. stream: {current_stream}, start: {start_time:.1f}, end: {current_time}, iv: {interpolated_vector}, cv: {curr_vector}")
+        target_list.append(curr_clip)
 
     start_time = current_time
     current_stream = cross_list[i]["next_stream"]
-    current_vector = cross_list[i]["vector_pairs"][0]["vector1"]
-    next_vector = cross_list[i] ["vector_pairs"][0]["vector2"]
+    prev_vector = cross_list[i]["vector_pairs"][0]["vector1"]
+    curr_vector = cross_list[i]["vector_pairs"][0]["vector2"]
 
     # cx, cy, cw, ch = current_vector
     # ix, iy, iw, ih = next_vector
     #
     # if move_x == 0 and move_y == 0:
-    #     tclip_list.append(sclip_list[current_stream].subclip(start_time, current_time))
+    #     target_list.append(stream_list[current_stream].subclip(start_time, current_time))
     # else:
     #     print(f"move x: {move_x} / move y: {move_y}")
-    #     temp_clip = sclip_list[current_stream].subclip(start_time, current_time)
+    #     temp_clip = stream_list[current_stream].subclip(start_time, current_time)
     #     moved_clip = temp_clip.set_position((move_x, move_y))
     #     composite_clip = CompositeVideoClip([moved_clip])
     #     # composite_clip = CompositeVideoClip([temp_clip, moved_clip])
-    #     tclip_list.append(composite_clip)
+    #     target_list.append(composite_clip)
 
     # current_stream = next_stream
     # start_time = current_time
@@ -134,23 +160,23 @@ for i in range(num_cross):
     # # move_y = cy-iy + move_y
 
 
-# tclip_list.append(sclip_list[current_stream].subclip(start_time, init_time + total_duration))
+# target_list.append(stream_list[current_stream].subclip(start_time, init_time + total_duration))
 
-print(len(tclip_list))
-for i, clip in enumerate(tclip_list):
+print(len(target_list))
+for i, clip in enumerate(target_list):
     print(f"Clip {i} duration: {clip.duration}")
 
-concatenated_clip = concatenate_videoclips(tclip_list)
-if total_duration == int(sclip_list[0].duration):
-    final_video = concatenated_clip.set_audio(sclip_list[0].audio)
+concatenated_clip = concatenate_videoclips(target_list)
+if total_duration == int(stream_list[0].duration):
+    final_video = concatenated_clip.set_audio(stream_list[0].audio)
 else:
-    final_video = concatenated_clip.set_audio(sclip_list[0].subclip(0, total_duration).audio)
+    final_video = concatenated_clip.set_audio(stream_list[0].subclip(0, total_duration).audio)
 
 output_path = f'data/simple_video_{random.randint(0, 999):03d}.mp4'
 final_video.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
-for i in range(len(sclip_list)):
-    sclip_list[i].close()
+for i in range(len(stream_list)):
+    stream_list[i].close()
 concatenated_clip.close()
 final_video.close()
 
