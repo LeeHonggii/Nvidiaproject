ada294af05e1edebb222d6e385c5092b8001e74f
Eddie9858 <birth0508@gmail.com>
2024-06-13 19:13:44 +0900

    test

--- video_insightface_main.py
+++ video_insightface_main.py
@@ -47,31 +47,59 @@ def process_video(video_path):
             break
 
         frame_count += 1
-        current_frame_eye_data = []
+        best_face = None
+        min_distance = float("inf")
+        max_area = 0
 
-        if frame_count % 5 == 0:
+        if frame_count % 5 == 0:  # Process every 5th frame
             faces = app.get(frame)
-            current_frame_positions = []
+            num_faces = len(faces)
 
-            for face in faces:
-                bbox = face["bbox"].astype(int)
+            if num_faces <= 3:
+                # Select the largest face when there are exactly three faces
+                for face in faces:
+                    bbox = face["bbox"].astype(int)
+                    x, y, w, h = bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    area = w * h
+                    if area > max_area:
+                        max_area = area
+                        best_face = face
+            elif num_faces > 3:
+                # Prioritize the face closest to the center when there are more than three faces
+                for face in faces:
+                    bbox = face["bbox"].astype(int)
+                    x, y, w, h = bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    distance = (
+                        (x + w / 2 - width / 2) ** 2 + (y + h / 2 - height / 2) ** 2
+                    ) ** 0.5
+                    if distance < min_distance:
+                        min_distance = distance
+                        best_face = face
+
+            if best_face:
+                bbox = best_face["bbox"].astype(int)
                 x, y, w, h = bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                current_frame_positions = [(x, y, w, h)]
 
-                if "landmark_2d_106" in face:
-                    lmk = face["landmark_2d_106"]
+                if "landmark_2d_106" in best_face:
+                    lmk = best_face["landmark_2d_106"]
                     lmk = np.round(lmk).astype(np.int64)
+                    current_frame_face_data = [lmk.tolist()]
+                    eye_point1 = tuple(lmk[35])
+                    eye_point2 = tuple(lmk[93])
+                    current_frame_eye_data = [(eye_point1, eye_point2)]
+
                     for point in lmk:
                         cv2.circle(
                             frame, tuple(point), 3, (200, 160, 75), 1, cv2.LINE_AA
                         )
+                    cv2.line(frame, eye_point1, eye_point2, (255, 0, 0), 2)
 
-                    current_frame_positions.append((x, y, w, h))
-                    current_frame_eye_data.append((lmk[35], lmk[93]))
-                    cv2.line(frame, tuple(lmk[35]), tuple(lmk[93]), (255, 0, 0), 2)
-
-            face_positions.append(current_frame_positions)
-            eye_endpoint.append(current_frame_eye_data)
+                face_positions.append(current_frame_positions)
+                eye_endpoint.append(current_frame_eye_data)
+                face_recognitions.append(current_frame_face_data)
 
             if display_scale_factor != 1:
                 display_frame = cv2.resize(
@@ -90,8 +118,26 @@ def process_video(video_path):
 
     cap.release()
     cv2.destroyAllWindows()
+    print(face_positions)
+    print(total_frames // 5)
+    print(len(face_positions))
 
     return face_positions, eye_endpoint, face_recognitions, fps, total_frames, duration
+
+
+def position_decision(x, y, w, h, width, height):
+    face_center_x = x + w / 2
+    face_center_y = y + h / 2
+
+    screen_center_x = width / 2
+    screen_center_y = height / 2
+
+    distance = (
+        (face_center_x - screen_center_x) ** 2 + (face_center_y - screen_center_y) ** 2
+    ) ** 0.5
+
+    area = w * h
+    return distance, area
 
 
 def intersection_over_union(x1, y1, w1, h1, x2, y2, w2, h2):
@@ -173,26 +219,21 @@ def find_matching_faces(
             print(f"Skipping frame {frame_index*5 + 1} due to missing eye data.")
             continue  # Skip frames without valid eye endpoint data
 
-        for i in range(len(frame_faces1)):
-            if i >= len(
-                eye_endpoints1[frame_index]
-            ):  # Check if eye endpoint index is valid
-                continue  # Skip if no valid eye data for this index in video 1
-
-            for j in range(len(frame_faces2)):
-                if j >= len(
+        for i, face1 in enumerate(frame_faces1):
+            for j, face2 in enumerate(frame_faces2):
+                if i < len(eye_endpoints1[frame_index]) and j < len(
                     eye_endpoints2[frame_index]
-                ):  # Check if eye endpoint index is valid
-                    continue  # Skip if no valid eye data for this index in video 2
+                ):
+                    x1, y1, w1, h1 = face1
+                    x2, y2, w2, h2 = face2
+                    iou_score = intersection_over_union(x1, y1, w1, h1, x2, y2, w2, h2)
+                    if iou_score > 0.6:
 
-                x1, y1, w1, h1 = frame_faces1[i]
-                x2, y2, w2, h2 = frame_faces2[j]
-                iou_score = intersection_over_union(x1, y1, w1, h1, x2, y2, w2, h2)
-
-                if iou_score > 0.6:
-                    current_frame = (frame_index + 1) * 5
-                    matched_faces.append(current_frame)
-                    print(f"Matched Face at Frame {current_frame}: IOU {iou_score:.2f}")
+                        current_frame = (frame_index) * 5
+                        matched_faces.append(current_frame)
+                        print(
+                            f"Matched Face at Frame {current_frame}: IOU {iou_score:.2f}"
+                        )
 
     return matched_faces
 
@@ -214,13 +255,14 @@ def create_json_structure(
     # Define meta information
     meta_info = {
         "num_stream": len(video_paths),
+        "metric": "time",
         "frame_rate": fps1,
         "num_frames": total_frames1,  # Example value
         "init_time": 0,
         "duration": duration1,  # Example value
         "num_vector_pair": 3,  # Example, adjust based on your actual data
         "num_cross": len(matched_faces),  # Example, adjust based on your actual data
-        "first_stream": 0,  # Example value
+        "first_stream": 1,
         "folder_path": folder_path,
     }
 
@@ -235,22 +277,24 @@ def create_json_structure(
     cross_points = []
 
     for idx, frame_id in enumerate(matched_faces):
-        index1 = frame_id // 5  # Assuming 5 is your sampling interval
-        index2 = index1
+        index1 = (
+            frame_id // 5
+        ) - 1  # Adjust index to match the eye_endpoint list indexing
+        index2 = index1  # Assuming the frames are synchronized in both videos
 
-        if index1 < len(eye_endpoints1) and index2 < len(eye_endpoints2):
+        if 0 <= index1 < len(eye_endpoints1) and 0 <= index2 < len(eye_endpoints2):
             if eye_endpoints1[index1] and eye_endpoints2[index2]:
                 vector_1 = (
-                    eye_endpoints1[index1][0][0].tolist()
-                    + eye_endpoints1[index1][0][1].tolist()
+                    np.array(eye_endpoints1[index1][0][0]).tolist()
+                    + np.array(eye_endpoints1[index1][0][1]).tolist()
                 )
                 vector_2 = (
-                    eye_endpoints2[index2][0][0].tolist()
-                    + eye_endpoints2[index2][0][1].tolist()
+                    np.array(eye_endpoints2[index2][0][0]).tolist()
+                    + np.array(eye_endpoints2[index2][0][1]).tolist()
                 )
 
                 cross_point = {
-                    "frame_id": frame_id * time_conversion,
+                    "time_stamp": frame_id / fps1,
                     "next_stream": next_stream,
                     "vector_pairs": [{"vector1": vector_1, "vector2": vector_2}],
                 }
@@ -339,7 +383,7 @@ if __name__ == "__main__":
     # Write the JSON structure to a file
     output_file = "output.json"
     write_json_file(json_structure, output_file)
-    print(eye_endpoint1[135])
-    print(eye_endpoint2[135])
+    print(eye_endpoint1[107])
+    print(eye_endpoint2[107])
 
     print(f"JSON file '{output_file}' has been written with the video analysis data.")
